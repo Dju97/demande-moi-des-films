9758110129bcfaf84ef229e2727359cca94da48e
Julien Duquesne <ju.duquesne@hotmail.fr>
2017-12-07 21:51:24 +0100

    Algo de recommandation v1

--- app/recommendation.py
+++ app/recommendation.py
@@ -56,18 +56,37 @@ class Recommendation:
 
     # Display the recommendation for a user
     def make_recommendation(self, user):
-        movie = choice(list(self.movies.values())).title
-
+        list_movies =[]
+        best_user = get_best_user(self,user)
+        for rating in self.ratings:
+            if rating.user == best_user and rating.is_appreciated == True :
+                list_movies.append(rating.movie)
+        movie = choice(list_movies).title
         return "Vos recommandations : " + ", ".join([movie])
 
     # Compute the similarity between two users
     @staticmethod
     def get_similarity(user_a, user_b):
-        return 1
+        liste=[]
+            for rating1 in self.ratings:
+                if rating1.user == user_a :
+                     movie = rating1.movie
+                     for rating2 in self.ratings:
+                         if rating2.user == user_b and rating2.movie == movie:
+                             liste.append(int(rating1.is_appreciated == rating2.is_appreciated))
+        return sum(liste)/len(liste)
 
     # Compute the similarity between a user and all the users in the data set
     def compute_all_similarities(self, user):
-        return []
+            similarities = []
+            for users in self.test_users:
+                similarities.append((get_similarity(users,user),users))
+        return similarities
+
+    def get_best_user(self,user):
+        similarities = compute_all_similarities(self,user)
+        similarities.sort()
+        return(similarities[-1][1])
 
     @staticmethod
     def get_best_movies_from_users(users):
